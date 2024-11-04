2f5fe51c9e0d692321e390e9896dd17e2034cf4e
Paveen <121075863+paveenH@users.noreply.github.com>
2024-11-04 22:50:44 +0800

    Update main.py

--- hyde-base/main.py
+++ hyde-base/main.py
@@ -1,9 +1,13 @@
 import transformers
 import torch
 from transformers import GenerationConfig
+from transformers import AutoModelForCausalLM, AutoTokenizer
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import logging
+import os
+
+from prediction_function import get_embedding_from_generation, probe_generation
 
 # Set up logging
 logging.basicConfig(filename='lm_backend.log', level=logging.ERROR,
@@ -18,22 +22,72 @@ class ChatRequest(BaseModel):
 
 # Load the model and pipeline only once when the server starts
 model_id = "meta-llama/Meta-Llama-3.1-8B-Instruct"
+probe_name = "meta-llama_Llama-3.1-8B-Instruct_4_all-conj.pt"
+probe_path = os.path.join(os.getcwd(), probe_name)
+layer = -4
 
-pipeline = transformers.pipeline(
-    "text-generation",
-    model=model_id,
-    model_kwargs={"torch_dtype": torch.bfloat16},
-    device=2
-)
+####################################################################################
+# pipeline = transformers.pipeline(
+#     "text-generation",
+#     model=model_id,
+#     model_kwargs={"torch_dtype": torch.bfloat16},
+#     device=2
+# )
 
-def make_request(message):
-    return [{"role": "user", "content": message}]
+# pipeline = transformers.pipeline(
+#     "text-generation",
+#     model=model_id,
+#     model_kwargs={"torch_dtype": torch.bfloat16},
+#     device_map="auto"
+# )
+
+# def make_request(message):
+#     return [{"role": "user", "content": message}]
+
+# @app.post("/generate")
+# async def generate_text(request: ChatRequest):
+#     try:
+#         outputs = pipeline(make_request(request.message), **request.gen_config)
+#         return outputs
+#     except Exception as e:
+#         logger.exception(f"Error in generate_text: {str(e)}")
+#         raise HTTPException(status_code=500, detail=str(e))
+####################################################################################
+
+try:
+    tokenizer = AutoTokenizer.from_pretrained(model_id)
+    model = AutoModelForCausalLM.from_pretrained(
+        model_id,
+        # torch_dtype=torch.bfloat16,
+        torch_dtype=torch.float16,
+        device_map="auto",
+        output_hidden_states=True
+    )
+    model.eval()
+except Exception as e:
+    logger.exception(f"Error loading model or tokenizer: {str(e)}")
+    raise e
 
 @app.post("/generate")
 async def generate_text(request: ChatRequest):
     try:
-        outputs = pipeline(make_request(request.message), **request.gen_config)
-        return outputs
+        results = get_embedding_from_generation(
+            message=request.message,
+            gen_config=request.gen_config,
+            model=model,
+            tokenizer=tokenizer,
+            device=model.device,
+            layer=layer  
+        )
+        
+        response = probe_generation(
+            probe_path=probe_path,
+            probe_name=probe_name,
+            model=model,
+            tokenizer=tokenizer,
+            results=results
+        )
+        return response
     except Exception as e:
         logger.exception(f"Error in generate_text: {str(e)}")
         raise HTTPException(status_code=500, detail=str(e))
